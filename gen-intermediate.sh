b40e618071a250a8a042f4605e5ad84d9aaa68c6
Martin Grabmueller <martin@tortoise>
2016-05-30 15:58:41 +0200

    Add certificate chain creation.

--- gen-intermediate.sh
+++ gen-intermediate.sh
@@ -8,3 +8,7 @@ chmod 400 intermediate/private/intermediate.key.pem
 openssl req -config intermediate/openssl.cnf -new -sha256 \
       -key intermediate/private/intermediate.key.pem \
       -out intermediate/csr/intermediate.csr.pem
+
+cat intermediate/certs/intermediate.cert.pem \
+    certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
+chmod 444 intermediate/certs/ca-chain.cert.pem
