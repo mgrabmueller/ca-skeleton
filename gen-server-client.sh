ad9cf0744204a2178d8379c35f86360579b46bb0
Martin Grabmueller <martin@grabmueller.de>
2017-06-20 11:21:26 +0200

    Make end user cert name configurable, put index.txt.attr files into VC.

--- gen-server-client.sh
+++ gen-server-client.sh
@@ -2,30 +2,32 @@
 
 set -e
 
-uuid=$(uuidgen)
+# Do 'sudo apt install uuid' if necessary.
+uuid=$(uuid -v4)
+cn=${1:-$uuid}
 
 # Generate server/client key.
 openssl genrsa -aes256 \
 	-passout file:secrets/server-client-secret \
-	-out intermediate/private/$uuid.key.pem 2048
-chmod 400 intermediate/private/$uuid.key.pem
+	-out intermediate/private/$cn.key.pem 2048
+chmod 400 intermediate/private/$cn.key.pem
 
 # Generate client/server CSR.
-export COMMON_NAME=$uuid
+export COMMON_NAME=$cn
 openssl req -config intermediate/openssl.cnf \
 	-passin file:secrets/server-client-secret \
-	-key intermediate/private/$uuid.key.pem \
-	-new -sha256 -out intermediate/csr/$uuid.csr.pem
+	-key intermediate/private/$cn.key.pem \
+	-new -sha256 -out intermediate/csr/$cn.csr.pem
 
 # Sign client/server CSR to get certificate.
 openssl ca -batch -config intermediate/openssl.cnf \
 	-extensions server_client_cert -days 375 -notext -md sha256 \
 	-passin file:secrets/intermediate-secret \
-	-in intermediate/csr/$uuid.csr.pem \
-	-out intermediate/certs/$uuid.cert.pem
-chmod 444 intermediate/certs/$uuid.cert.pem
+	-in intermediate/csr/$cn.csr.pem \
+	-out intermediate/certs/$cn.cert.pem
+chmod 444 intermediate/certs/$cn.cert.pem
 
 echo "Server/Client certificate generated."
 echo "Use the following command to copy the files needed for deployment:"
-echo "   cp intermediate/certs/$uuid.cert.pem intermediate/certs/ca-chain.cert.pem intermediate/private/$uuid.key.pem DEST"
+echo "   cp intermediate/certs/$cn.cert.pem intermediate/certs/ca-chain.cert.pem intermediate/private/$cn.key.pem DEST"
 
