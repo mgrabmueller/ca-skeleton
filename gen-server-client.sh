e1df7e69770b9d6709cb9221c9a5e08cd737a241
Martin Grabmueller <martin@grabmueller.de>
2017-06-21 16:08:22 +0200

    Generate server/client certs with longer CNs.

--- gen-server-client.sh
+++ gen-server-client.sh
@@ -12,8 +12,13 @@ openssl genrsa -aes256 \
 	-out intermediate/private/$cn.key.pem 2048
 chmod 400 intermediate/private/$cn.key.pem
 
+openssl rsa -in intermediate/private/$cn.key.pem \
+	-passin file:secrets/server-client-secret \
+	-out intermediate/private/$cn.nopw.key.pem
+chmod 400 intermediate/private/$cn.nopw.key.pem
+
 # Generate client/server CSR.
-export COMMON_NAME=$cn
+export COMMON_NAME=$cn.peer.example.com
 openssl req -config intermediate/openssl.cnf \
 	-passin file:secrets/server-client-secret \
 	-key intermediate/private/$cn.key.pem \
@@ -29,5 +34,5 @@ chmod 444 intermediate/certs/$cn.cert.pem
 
 echo "Server/Client certificate generated."
 echo "Use the following command to copy the files needed for deployment:"
-echo "   cp intermediate/certs/$cn.cert.pem intermediate/certs/ca-chain.cert.pem intermediate/private/$cn.key.pem DEST"
+echo "   cp intermediate/certs/$cn.cert.pem intermediate/certs/ca-chain.cert.pem intermediate/private/$cn.key.pem intermediate/private/$cn.nopw.key.pem DEST"
 
